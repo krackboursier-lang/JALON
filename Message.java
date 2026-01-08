908e9a4b78977e66026acda7d9e96af645187117
Krackboursier-lang <Krackboursier@gmail.com>
2026-01-08 09:06:20 +0100

    konda

---
+++ Message.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+        public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String VotreNom;
+        
+        System.out.println("Quel est votre nom ? :");
+        VotreNom = sc.nextLine();
+         System.out.println("\n Bonjour "+VotreNom+", enchanté !");
+
+         sc.close();
+
+    }
+    
+}
+
